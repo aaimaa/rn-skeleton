c2fe9fdd757b1313fa777afee8d2a9c81d113ba9
Pratham Meghani <premmeghani16@gmail.com>
2021-04-21 15:44:48 +0530

    SubmitButtonAdded

--- Screens/AboutUs.js
+++ Screens/AboutUs.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, View ,Image} from 'react-native'
 import { Rating, AirbnbRating } from 'react-native-elements';
+import {Button} from 'native-base'
 import { useFonts } from 'expo-font';
 const AboutUs = () => {
     const [loaded] = useFonts({
@@ -25,8 +26,14 @@ const AboutUs = () => {
                 lot and planning to do more in future.
             </Text>
             </View>
-            <View style={{marginTop:100}}>
+            <View style={{marginTop:60}}>
+                <Text style={{fontSize:24,color:'white',textAlign:'center'}}>Rate Us</Text>
             <Rating type="star" showRating={true} fractions={2}/>
+            </View>
+            <View style={{margin:20}}>
+            <Button style={{backgroundColor:'green',width:100,alignSelf:'center',justifyContent:'center',borderRadius:12}}>
+                <Text>Submit</Text>
+            </Button>
             </View>
         </View>
     )
