327d0cfb74ac25db57343d02e6ae8573747d707f
Pratham Meghani <premmeghani16@gmail.com>
2021-04-21 15:54:02 +0530

    FontUpdated

--- Screens/AboutUs.js
+++ Screens/AboutUs.js
@@ -5,7 +5,7 @@ import {Button} from 'native-base'
 import { useFonts } from 'expo-font';
 const AboutUs = () => {
     const [loaded] = useFonts({
-        DancingScript: require('../assets/fonts/DancingScript-SemiBold.ttf'),
+        Cairo: require('../assets/fonts/Cairo-SemiBold.ttf'),
       });
       if (!loaded) {
         return null;
@@ -19,12 +19,14 @@ const AboutUs = () => {
          
             <Text style={{color:'white',fontSize:24,fontStyle:'italic',fontWeight:'bold'}}>Avengers Corp</Text>
             </View>
-            <Text style={{fontFamily:'DancingScript',color:'green',fontSize:18,lineHeight:30}}>Founded in 2008 and based in Delhi,India,XYZ is a trusted community and here to help millions of
+            <View style={{alignItems:'center'}}>
+            <Text style={{fontFamily:'Cairo',color:'green',fontSize:18,lineHeight:30}}>Founded in 2008 and based in Delhi,India,XYZ is a trusted community and here to help millions of
                 people in society.We are team of 500 individuals which are delighted to serve people like you.
 
                 Just some years ago,we became fascinated by idea of discovering the world through someone eyes.We have achieved
                 lot and planning to do more in future.
             </Text>
+            </View>
             </View>
             <View style={{marginTop:60}}>
                 <Text style={{fontSize:24,color:'white',textAlign:'center'}}>Rate Us</Text>
