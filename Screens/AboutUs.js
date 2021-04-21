5d069131d31a76991f059b6072dc6039fc39fd3f
Pratham Meghani <premmeghani16@gmail.com>
2021-04-21 15:23:03 +0530

    AvengersIconAdded

--- Screens/AboutUs.js
+++ Screens/AboutUs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,Image} from 'react-native'
 
 import { useFonts } from 'expo-font';
 const AboutUs = () => {
@@ -10,7 +10,13 @@ const AboutUs = () => {
         return null;
       }
     return (
+
         <View style={styles.container}>
+            <View style={{flexDirection:'row'}}>
+            <Image source={{uri:"https://img.icons8.com/color/48/000000/avengers.png"}}  style={styles.image}/>
+         
+            <Text style={{color:'white',fontSize:24,fontStyle:'italic',fontWeight:'bold'}}>Avengers Corp</Text>
+            </View>
             <Text style={{fontFamily:'DancingScript',color:'green',fontSize:18,lineHeight:30}}>Founded in 2008 and based in Delhi,India,XYZ is a trusted community and here to help millions of
                 people in society.We are team of 500 individuals which are delighted to serve people like you.
 
@@ -25,8 +31,12 @@ export default AboutUs
 
 const styles = StyleSheet.create({
     container:{
-        justifyContent:'center',
+       
         flex:1,
         backgroundColor:'#202020'
+    },
+    image:{
+width:46,
+height:46
     }
 })
